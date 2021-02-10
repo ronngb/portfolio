82fe920789022e5bc429d52fdad2f3c9e26d321c
ronngb <ronny.gabi@gmail.com>
2021-02-10 19:38:11 +0800

    Created a template for HOME page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
     <Navbar></Navbar>
+    <div class="container">
+      <home></home>
+    </div>
   </div>
 </template>
 <script>
 import Navbar from './components/Navbar.vue'
+import home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    home
   }
 }
 </script>
 <style>
-#app {}
+#app {
+  font-family: Neutra Text, sans-serif;
+  font-size: 20px;
+  font-weight: 700;
+}
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="row align-items-center">
+    <!-- TODO: Increase the H1 font size -->
+    <div class="col-md-6 intro ">
+      <h1>Ronn Gabiosa</h1>
+      <!-- TODO: Increase the P font size  -->
+      <p>Hi there! I'm Ronn, your friendly neighborhood developer, who enjoys designing and developing websites, great in both Front and Back end.</p>
+    </div>
+    <div class="col-md-6 ">
+      <img width="500" height="500" src="../assets/pics.jpg" alt="">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Navbar",
+  components: {},
+};
+</script>
+<style>
+.intro {}
+
+.intro h1 {
+  text-align: center;
+  font-size: 5rem;
+  font-weight: bold;
+  color: #00a89c;
+}
+
+.intro p {
+  font-size: 1.5em;
+  /*text-align: left;*/
+  font-weight: 200;
+  line-height: 1;
+}
+</style>
