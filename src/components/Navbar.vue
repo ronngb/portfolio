f49de0582bf6690bb228d79940f41f2ca4c9f0a4
ronngb <ronny.gabi@gmail.com>
2021-03-03 15:10:04 +0800

    created hover effects on navbar menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,5 @@
 <template>
-  <!-- TODO: Create github acct. also new vue project-->
-  <nav class="navbar navbar-expand-md navbar-light bg-light">
+  <nav class="navbar navbar-expand-md navbar-light">
     <div class="container">
       <a class="navbar-brand">
         <img class="" width="30" height="30" src="./../assets/logo.png" />
@@ -10,17 +9,18 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div id="navbarNav" class="collapse navbar-collapse d-md-block justify-content-md-end justify-content-center">
-        <ul id="main-nav" class="navbar-nav justify-content-center">
-          <li class="nav-item active">
+        <ul id="nav-links" class="navbar-nav justify-content-center">
+          <!-- TODO: after clicking the anchor tag add .active class -->
+          <li class="nav-item ">
             <a class="nav-link" href="#">HOME</a>
           </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="#about">ABOUT</a>
+          <li class="nav-item ">
+            <a class="nav-link" href="#">ABOUT</a>
           </li>
-          <li class="nav-item active">
+          <li class="nav-item ">
             <a class="nav-link" href="#">PROJECTS</a>
           </li>
-          <li class="nav-item active">
+          <li class="nav-item ">
             <a class="nav-link" href="#">RESUME</a>
           </li>
         </ul>
@@ -35,8 +35,11 @@ export default {
 };
 </script>
 <style>
-.nav-cont {
-  margin: 0px 15px !important;
+#app {
+  font-family: Neutra Text, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  --teal: #00a89c;
 }
 
 /*Add this rules in portrait*/
@@ -45,13 +48,40 @@ export default {
   /*height: calc(100vh - 45px);*/
 }
 
-#main-nav li {
+.nav-cont {
+  margin: 0px 15px !important;
+}
+
+#nav-links li {
   padding: 0px 15px;
 }
 
-#main-nav a {
-  font-family: Neutra Text, sans-serif;
+#nav-links>li>a {
+  position: relative;
   font-size: 20px;
   font-weight: 700;
+  color: black;
+}
+
+#nav-links>li>a:hover,
+#nav-links>li>a:focus {
+  color: var(--teal);
+}
+
+#nav-links>li>a::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 4px;
+  bottom: 7px;
+  left: 0;
+  background-color: var(--teal);
+  border-radius: 25px;
+  transform: scaleX(0);
+  transition: all 0.12s ease-in-out 0s;
+}
+
+#nav-links>li>a:hover::before {
+  transform: scaleX(1);
 }
 </style>
