44e5c989641a3b704570cd5617e21b9141646d8e
ronngb <ronny.gabi@gmail.com>
2021-06-22 22:44:24 +0800

    set the projects list to grid layout

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,76 +1,101 @@
 <template>
-  <div class="row justify-content-around">
-    <div class="col-md-12 proj">
-      <h1>Project</h1>
+  <section class="projects">
+    <div class="content-wrapper">
+      <h1>Projects</h1>
+      <section class="projects-list">
+        <ul>
+          <article>
+            <li>
+              <figure class="project-image">
+                <img src="../assets/test-proj.png" alt="" />
+              </figure>
+              <h2 class="project-title">Fridge Man</h2>
+              <p class="project-info">
+                Fridge Man was built to make food inventory management a breeze.
+                Fridge Man allows you to create and edit locations in which food
+                will be stored. You can also...
+              </p>
+            </li>
+          </article>
+          <article>
+            <li>
+              <figure class="project-image">
+                <img src="../assets/test-proj.png" alt="" />
+              </figure>
+              <h2 class="project-title">Fridge Man</h2>
+              <p class="project-info">
+                Fridge Man was built to make food inventory management a breeze.
+                Fridge Man allows you to create and edit locations in which food
+                will be stored. You can also...
+              </p>
+            </li>
+          </article>
+        </ul>
+      </section>
     </div>
-    <!-- TODO: font-family set to current -->
-    <div class="col-md-6">
-      <ul class="proj-list">
-        <li class="d-flex align-items-center flex-column">
-          <div class=" image">
-            <!-- PROB: image-->
-            <img class="img-fluid" src="../assets/test-proj.png" alt="">
-          </div>
-          <div class="text">
-            <h2>Fridge Man</h2>
-            <p>Fridge Man was built to make food inventory management a breeze. Fridge Man allows you to create and edit locations in which food will be stored. You can also...</p>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>
+  </section>
 </template>
 <script>
 export default {
-  name: "Project",
+  name: 'Projects',
   components: {},
 };
 </script>
 <style>
-.proj h1 {
-  padding: 5.5rem 0 3rem;
+.projects h1 {
   text-align: center;
-  font-size: 7.6rem;
-  font-weight: bold;
-  color: #00a89c;
+  font-size: 6.5rem;
+  font-weight: 600;
+  color: #00a69a;
 }
 
-.proj-list {
+.projects-list {
+  margin: 50px 0px;
+}
+
+.projects-list ul {
+  display: grid;
   list-style: none;
-  padding-left: 0;
+  grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+  grid-template-rows: auto;
+  padding-inline-start: 0px;
+  gap: 70px 60px;
 }
 
-.proj-list li {
-  /*height: 500px;
-width: 450px;*/
-  display: inline-block;
-  padding: 0px 20px;
-  border-radius: 0.5em;
-  box-shadow: 0 0 20px rgba(0, 0, 0, .16);
+.projects-list figure {
+  margin-bottom: 0px;
 }
 
-.text {
-
-  padding-bottom: 1.5rem;
-  margin: auto;
+.projects-list li {
+  height: 350px;
+  padding: 0px 30px;
+  border-radius: 8px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.16);
 }
 
-.text h2 {
-  font-size: 1.75em;
-  font-weight: bold;
+.project-title {
+  font-size: 1.7rem;
+  font-weight: 600;
 }
 
-.text p {
-  margin: 0;
-  font-weight: normal;
-  line-height: 1
+.project-info {
+  font-family: 'Josefin Sans';
+  font-weight: 100;
+  line-height: 1;
+  margin-bottom: 0px;
 }
 
-.image {
+.project-image img {
   position: relative;
-  bottom: 2em;
-  border-radius: 0.5em;
-  box-shadow: 0 0 20px rgba(0, 0, 0, .16);
-  width: 98%;
+  bottom: 1.6rem;
+  border-radius: 8px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.16);
+  max-width: 100%;
+}
+
+@media (max-width: 768px) {
+  .projects-list ul {
+    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  }
 }
 </style>
