1f58bec250a4d73301a23df390ffd8096b3b55aa
ronngb <ronny.gabi@gmail.com>
2021-07-04 14:11:07 +0800

    refactor css and reformat html

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,42 +1,32 @@
 <template>
-  <header v-scroll="navbarTransition" class="sticky-top">
-    <nav class="navbar navbar-expand-md navbar-light bg-white">
-      <div class="container">
-        <a class="navbar-brand">
-          <figure class="logo" style="margin-bottom: 0rem;">
-            <img alt="website logo" src="./../assets/logo.png" />
-          </figure>
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          @click="showMenu = !showMenu"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div
-          id="navbar_list-item"
-          class="d-md-block"
-          :class="{ active: showMenu }"
-        >
-          <ul id="nav-links" class="navbar-nav">
-            <li class="nav-item ">
-              <a class="nav-link" href="#">HOME</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="#about" @click="showMenu = false">
-                ABOUT
-              </a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="#">PROJECTS</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="#">RESUME</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+  <header v-scroll="navbarTransition">
+    <nav class="">
+      <a>
+        <figure class="website-logo">
+          <img alt="website logo" src="./../assets/logo.png" />
+        </figure>
+      </a>
+      <button class="toggle-button" type="button" @click="showMenu = !showMenu">
+        <svg class="toggle-icon" viewBox="0 0 60 60">
+          <path d="M1 9 H58 M20 29 H58 M38 49 H58" />
+        </svg>
+      </button>
+      <ul :class="{ active: showMenu }">
+        <li class="nav-item">
+          <a href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a href="#about" @click="showMenu = false">
+            ABOUT
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#">PROJECTS</a>
+        </li>
+        <li class="nav-item">
+          <a href="#">RESUME</a>
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
@@ -51,8 +41,8 @@ export default {
     navbarTransition(evt, el) {
       let cssProperty =
         window.scrollY == 0
-          ? 'padding: 1rem 0rem;'
-          : 'padding: .5rem 0rem;box-shadow: 0 0 20px rgba(0,0,0,.16);';
+          ? 'padding: 1.3rem 0rem;'
+          : 'padding: 1rem 0rem;box-shadow: 0 0 20px rgba(0,0,0,.16);';
 
       if (window.screen.width > 768) {
         el.setAttribute('style', cssProperty);
@@ -62,46 +52,86 @@ export default {
 };
 </script>
 <style>
-/*header*/
 header {
+  position: sticky;
+  top: 0;
+  z-index: 100;
   background-color: white;
   padding: 1rem 0rem;
   transition: all 0.2s ease-in-out;
 }
 
-/*navbar*/
-#nav-links li {
-  padding: 0px 15px;
+header nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0px 9%;
 }
 
-#nav-links li > a {
-  position: relative;
-  font-size: 1.2rem;
-  font-weight: 700;
-  color: black;
+.website-logo {
+  margin-bottom: 0rem;
 }
 
-.logo img {
+.website-logo img {
   width: 30px;
   height: 30px;
 }
 
-@media (max-width: 768px) {
+nav ul {
+  display: flex;
+  list-style: none;
+  padding-left: 0;
+  margin-bottom: 0;
+}
+
+nav .toggle-button {
+  padding: 0.25rem 0.75rem;
+  background-color: transparent;
+  border: 1px solid #c5c3c6;
+  border-radius: 0.25rem;
+}
+
+.toggle-icon {
+  width: 30px;
+  height: 30px;
+}
+
+.toggle-icon path {
+  stroke: #46494c;
+  stroke-width: 3;
+}
+
+nav ul > li {
+  padding: 0 1.6rem;
+}
+
+.nav-item a {
+  position: relative;
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: black;
+  padding: 0.5rem 0rem;
+  display: block;
+}
+
+@media screen and (max-width: 768px) {
   header {
-    padding: 0rem;
+    padding: 0rem !important;
     border-bottom: 1px solid rgba(0, 0, 0, 0.16);
   }
 
-  #navbar_list-item {
-    display: flex;
+  header nav {
+    padding: 8px 10px;
+  }
+
+  nav ul {
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     position: fixed;
-    left: 0;
     top: 57px;
-    z-index: 100;
     opacity: 0;
-    border-top: 1px solid rgba(0, 0, 0, 0.16);
     background-color: white;
     height: calc(100vh - 57px);
     width: 100vw;
@@ -109,25 +139,31 @@ header {
     transform: translateX(-20rem);
   }
 
-  #navbar_list-item.active {
+  nav ul.active {
     opacity: 1;
     transform: translateX(0px);
   }
 
-  #nav-links li > a {
+  .nav-item a {
     font-size: 1.4rem;
     text-align: center;
   }
 }
 
-@media (min-width: 768px) {
-  #nav-links li > a:hover,
-  #nav-links li > a:focus {
+@media screen and (min-width: 769px) {
+  nav .toggle-button {
+    display: none;
+  }
+  nav ul {
+    flex: 0 0 45%;
+  }
+  .nav-item a:hover,
+  .nav-item a:focus {
+    text-decoration: none;
     color: #00a69a;
   }
 
-  /*Recreate*/
-  #nav-links li > a::before {
+  .nav-item a::before {
     content: '';
     position: absolute;
     width: 100%;
@@ -140,7 +176,7 @@ header {
     transition: all 0.12s ease-in-out;
   }
 
-  #nav-links li > a:hover::before {
+  .nav-item a:hover::before {
     transform: scaleX(1);
   }
 }
