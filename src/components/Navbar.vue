0d2625af11c6584d9c08c20771e2b73bf4235e79
ronngb <ronny.gabi@gmail.com>
2021-09-17 15:22:30 +0800

    refactor and set Navbar.vue to mobile 1st

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,9 +23,9 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#project" @click="showMenu = false"
-            >PROJECTS</a
-          >
+          <a class="nav-link" href="#project" @click="showMenu = false">
+            PROJECTS
+          </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">RESUME</a>
@@ -40,6 +40,10 @@ export default {
     scroll: {
       inserted(el, binding) {
         window.addEventListener('scroll', () => {
+          binding.value(el);
+        });
+
+        window.addEventListener('resize', () => {
           binding.value(el);
         });
       },
@@ -59,6 +63,8 @@ export default {
 
       if (window.screen.width > 768) {
         el.setAttribute('style', cssProperty);
+      } else {
+        el.setAttribute('style', 'padding: 0;');
       }
     },
   },
@@ -69,9 +75,8 @@ header {
   position: sticky;
   top: 0;
   z-index: 100;
-  padding: 1rem 0;
-  background-color: white;
-  transition: all 0.2s ease-in-out;
+  background-color: #fff;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.16);
 }
 
 header nav {
@@ -79,7 +84,7 @@ header nav {
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  padding: 0 9%;
+  padding: 8px 10px;
 }
 
 .website-logo {
@@ -89,8 +94,24 @@ header nav {
 
 .site-nav {
   display: flex;
-  margin-bottom: 0;
   padding-left: 0;
+  position: fixed;
+  top: 57px;
+  left: 0;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: calc(100vh - 57px);
+  background-color: white;
+  transform: translateX(-320px);
+  opacity: 0;
+  transition: opacity 0.2s ease-in-out, transform 0.5s ease-in-out;
+}
+
+.site-nav.active {
+  transform: translateX(0);
+  opacity: 1;
 }
 
 .toggle-button {
@@ -120,62 +141,48 @@ header nav {
   padding: 0.5rem 0;
   color: black;
   font-weight: 700;
-  font-size: 1.2rem;
+  font-size: 1.4rem;
 }
 
-@media screen and (max-width: 768px) {
+@media screen and (min-width: 768px) {
   header {
-    padding: 0 !important;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.16);
+    padding: 1rem 0;
+    transition: all 0.2s ease-in-out;
+    border-bottom: 0;
   }
 
   header nav {
-    padding: 8px 10px;
+    padding: 6px 9%;
   }
 
   .site-nav {
-    position: fixed;
-    top: 57px;
-    left: 0;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100vw;
-    height: calc(100vh - 57px);
-    background-color: white;
-    transform: translateX(-320px);
-    opacity: 0;
-    transition: opacity 0.2s ease-in-out, transform 0.5s ease-in-out;
-  }
-
-  .site-nav.active {
-    transform: translateX(0);
+    position: static;
+    flex-direction: row;
     opacity: 1;
+    height: 0;
+    transform: none;
   }
 
   .nav-link {
-    font-size: 1.4rem;
-    text-align: center;
+    font-size: 1.2rem;
   }
-}
 
-@media screen and (min-width: 769px) {
-  li.nav-item:nth-child(n + 1) {
+  .nav-item:nth-child(n + 1) {
     transform: translateY(-10px);
     opacity: 0;
     animation: navbar-trans 0.5s ease-out forwards;
   }
 
-  li.nav-item:nth-child(1) {
+  .nav-item:nth-child(1) {
     animation-delay: 0.1s;
   }
-  li.nav-item:nth-child(2) {
+  .nav-item:nth-child(2) {
     animation-delay: 0.3s;
   }
-  li.nav-item:nth-child(3) {
+  .nav-item:nth-child(3) {
     animation-delay: 0.5s;
   }
-  li.nav-item:nth-child(4) {
+  .nav-item:nth-child(4) {
     animation-delay: 0.7s;
   }
 
@@ -186,12 +193,12 @@ header nav {
   .site-nav {
     flex: 0 0 45%;
   }
-  a.nav-link:hover,
-  a.nav-link:focus {
+  .nav-link:hover,
+  .nav-link:focus {
     color: #00a69a;
   }
 
-  a.nav-link::before {
+  .nav-link::before {
     position: absolute;
     bottom: 7px;
     left: 0;
@@ -204,7 +211,7 @@ header nav {
     content: '';
   }
 
-  a.nav-link:hover::before {
+  .nav-link:hover::before {
     transform: scaleX(1);
   }
 }
