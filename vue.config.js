dfd059dd57cdacb20947be3ea364923a341e78a6
ronngb <ronny.gabi@gmail.com>
2021-09-18 19:28:10 +0800

    added additionalData

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  // pluginOptions: {
-  //   lintStyleOnBuild: false,
-  //   stylelint: {
-  //     fix: false, // boolean (default: true)
-  //     // string | [string] (default: ['src/**/*.{vue,htm,html,css,sss,less,scss}'])
-  //     files: 'src/**/*.{vue,htm,html,css,sss,less,scss}',
-  //   },
-  // },
   css: {
     loaderOptions: {
       scss: {
         additionalData: `
-          @import "@/scss/variables";          
+          @import "@/scss/variables.scss";
+          @import "@/scss/base.scss";          
         `,
       },
     },
