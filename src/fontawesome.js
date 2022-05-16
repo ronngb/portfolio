5a7906ff8158c07464f7fb4823439b788f37e90c
ronngb <ronny.gabi@gmail.com>
2022-05-16 18:03:56 +0800

    new: append new icon

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -8,8 +8,9 @@ import {
   faLinkedin,
   faGithub,
 } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faFontAwesome, faLinkedin, faGithub)
+library.add(faFontAwesome, faLinkedin, faGithub, faEnvelope)
 
 export default {
   FontAwesomeIcon,
