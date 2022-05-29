49b427833473731ca595d48b4fe52d78b541df32
ronngb <ronny.gabi@gmail.com>
2022-05-29 19:30:25 +0800

    rm: paragraph class

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -5,7 +5,7 @@
         <span class="primary-heading-main">Ronnel Gabiosa</span>
         <span class="primary-heading-sub">Web Developer</span>
       </h1>
-      <p class="paragraph">
+      <p class="intro-paragraph">
         Hi there! I'm Ronnel, your friendly neighborhood developer, who enjoys
         designing and developing websites, great in both Front and Back end.
       </p>
@@ -26,16 +26,19 @@
 export default { name: 'Intro' }
 </script>
 <style lang="scss" scoped>
-p {
-  padding-left: 5px;
-}
-
 section {
   place-items: center;
 
   @include responsive(md) {
     grid-template-columns: 55% 45%;
   }
+}
+
+.intro-paragraph {
+  opacity: 0;
+  padding-left: 5px;
+  font-size: clamp(2.128rem, calc(1.776rem + 1.1vw), 3.312rem);
+  animation: fadeInUp 0.5s ease-out 1s forwards;
 }
 
 .hero-image {
