ee715ab98d2464c247526b76b4971fe7b6606c31
ronngb <ronny.gabi@gmail.com>
2022-08-16 01:22:25 +0800

    chore: edit deploy.sh for github pages

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,9 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git commit -m 'new deploy'
+git push -f git@github.com:ronngb/portfolio master:gh-pages
 
 cd -
