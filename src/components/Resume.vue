0c8bd100eb2abad2f3dcc57a5b0df37bc8905822
ronngb <ronny.gabi@gmail.com>
2023-01-23 07:32:08 +0800

    remove redundant in resume component

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,21 +1,19 @@
 <template>
   <section id="resume" class="content-wrapper">
-    <h2 class="secondary-heading">Resume</h2>
-    <ul id="drawing" class="">
-      <div class="timeline"></div>
-      <li v-for="data in resume" class="">
-        <font-awesome-layers>
+    <h2 class="secondary-heading resume-heading">Resume</h2>
+    <ul class="resume-list">
+      <div class="resume-timeline"></div>
+      <li v-for="data in resume" class="resume-item">
+        <font-awesome-layers class="circle-icon">
           <font-awesome-icon
             icon="fas fa-circle"
-            :style="{ color: '#00a69a' }"
-          />
+            :style="{ color: '#00a69a' }" />
           <font-awesome-icon
             icon="fas fa-circle"
             transform="shrink-6"
-            inverse
-          />
+            inverse />
         </font-awesome-layers>
-        <h3 class="tertiary-heading">
+        <h3 class="resume-details-heading tertiary-heading">
           <span class="tertiary-heading-main">
             {{ data.position }}
           </span>
@@ -23,7 +21,7 @@
             <em>{{ data.company }}</em>
           </span>
         </h3>
-        <p>
+        <p class="resume-details-paragraph">
           <b>
             <font-awesome-icon icon="fas fa-location-dot" />
             {{ data.location }}
@@ -50,180 +48,188 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.timeline {
-  display: none;
-  position: absolute;
-  width: 5px;
-  height: 100%;
-  background-color: $color-primary;
-  @include responsive(sm) {
-    display: block;
-  }
-}
-
-ul {
-  padding: 0;
-  display: grid;
-  grid-template-columns: minmax(290px, 1fr);
-  gap: 30px 50px;
-  position: relative;
-  justify-items: center;
-  padding-bottom: 30px;
-  @include responsive(sm) {
-    grid-template-columns: repeat(2, minmax(290px, 1fr));
-  }
-}
-
-div[class~='fa-layers'] {
+.circle-icon {
   display: none;
   font-size: 2em;
   position: absolute;
   top: -10px;
+  opacity: 0;
+  transform: scale(0);
   @include responsive(sm) {
     display: block;
   }
 }
 
-li {
-  display: flex;
-  position: relative;
-  width: min(100%, 456px);
-  @include responsive(sm) {
+.resume {
+  &-timeline {
+    display: none;
+    position: absolute;
+    width: 5px;
+    height: 100%;
+    background-color: $color-primary;
+    transform: scale(0);
+    transform-origin: 50% 100%;
+    @include responsive(sm) {
+      display: block;
+    }
+  }
+
+  &-list {
+    padding: 0;
+    display: grid;
+    grid-template-columns: minmax(290px, 1fr);
+    gap: 30px 50px;
+    position: relative;
+    justify-items: center;
+    @include responsive(sm) {
+      grid-template-columns: repeat(2, minmax(290px, 1fr));
+      padding-bottom: 30px;
+    }
+  }
+
+  &-item {
+    display: flex;
+    opacity: 0;
+    position: relative;
+    width: min(100%, 456px);
+
     &:nth-of-type(odd) {
-      justify-self: end;
-      grid-column: 1/2;
+      transform-origin: 100% 50%;
+      transform: translateX(30px) scaleX(0.2);
+      @include responsive(sm) {
+        justify-self: end;
+        grid-column: 1/2;
 
-      & div[class~='fa-layers'] {
-        right: -35px;
-      }
+        .circle-icon {
+          right: -35px;
+        }
 
-      & p {
-        border-top-right-radius: 0;
+        p {
+          border-top-right-radius: 0;
+        }
       }
     }
-
     &:nth-of-type(even) {
-      justify-self: start;
-      grid-column: 2/-1;
+      transform-origin: 0% 50%;
+      transform: translateX(-30px) scaleX(0.2);
+      @include responsive(sm) {
+        justify-self: start;
+        grid-column: 2/-1;
 
-      & div[class~='fa-layers'] {
-        left: -35px;
-      }
+        .circle-icon {
+          left: -35px;
+        }
 
-      & h3 {
-        border-top-left-radius: 0;
+        h3 {
+          border-top-left-radius: 0;
+        }
       }
     }
 
-    &:last-of-type {
+    & > *:not(div) > * {
+      opacity: 0;
+      transform: translateY(10px);
+    }
+
+    &:not(li:last-of-type)::after {
+      content: '';
+      height: 30px;
       position: absolute;
-      top: 30px;
+      bottom: -30px;
+      left: 15px;
+      width: 5px;
+      background-color: $color-primary;
+      transform-origin: 50% 0;
+      transform: scale(0);
+      @include responsive(sm) {
+        display: none;
+      }
     }
 
-    @for $i from 1 through 4 {
-      &:nth-of-type(#{$i}) {
-        @if $i % 2 == 0 {
-          grid-row: $i #{'/'} ($i + 1);
-        } @else {
-          grid-row: $i #{'/'} ($i + 1);
+    @include responsive(sm) {
+      &:last-of-type {
+        position: absolute;
+        top: 30px;
+      }
+
+      @for $i from 1 through 4 {
+        &:nth-of-type(#{$i}) {
+          @if $i % 2 == 0 {
+            grid-row: $i #{'/'} ($i + 1);
+          } @else {
+            grid-row: $i #{'/'} ($i + 1);
+          }
         }
       }
     }
   }
-}
 
-h3 {
-  border-radius: 0.8rem 0 0 0.8rem;
-  padding: clamp(0.8rem, calc(0.704rem + 0.3vw), 1rem);
-  background-color: $color-quartet;
-  flex: 1 1 56%;
-}
+  &-details {
+    &-heading {
+      border-radius: 0.8rem 0 0 0.8rem;
+      padding: clamp(0.8rem, calc(0.704rem + 0.3vw), 1rem);
+      background-color: $color-quartet;
+      flex: 1 1 56%;
+    }
 
-p {
-  color: #fff;
-  padding: clamp(0.8rem, calc(0.704rem + 0.3vw), 1rem);
-  border-radius: 0 0.8rem 0.8rem 0;
-  background-color: $color-primary;
-  flex: 1 1 36%;
-  & > * {
-    display: inherit;
-  }
+    &-paragraph {
+      color: #fff;
+      padding: clamp(0.8rem, calc(0.704rem + 0.3vw), 1rem);
+      border-radius: 0 0.8rem 0.8rem 0;
+      background-color: $color-primary;
+      flex: 1 1 36%;
+      & > * {
+        display: inherit;
+      }
 
-  & :nth-child(1) {
-    font-size: clamp(1.28rem, calc(1.12rem + 0.48vw), 1.6rem);
-    line-height: 1.6;
-    font-style: italic;
-  }
-  & :nth-child(2) {
-    font-size: clamp(1.1rem, calc(0.96rem + 0.45vw), 1.4rem);
-    font-weight: 100;
-  }
-}
-
-// Animation Init Start
-.timeline {
-  transform: scale(0);
-  transform-origin: 50% 100%;
-}
-
-li {
-  opacity: 0;
-  &:nth-of-type(odd) {
-    transform-origin: 100% 50%;
-    transform: translateX(30px) scaleX(0.2);
-  }
-  &:nth-of-type(even) {
-    transform-origin: 0% 50%;
-    transform: translateX(-30px) scaleX(0.2);
-  }
-  & > *:not(div) > * {
-    opacity: 0;
-    transform: translateY(10px);
-  }
-
-  & div[class~='fa-layers'] {
-    opacity: 0;
-    transform: scale(0);
-  }
-
-  &:not(li:last-of-type)::after {
-    content: '';
-    height: 30px;
-    position: absolute;
-    bottom: -30px;
-    left: 15px;
-    width: 5px;
-    background-color: $color-primary;
-    transform-origin: 50% 0;
-    transform: scale(0);
-    @include responsive(sm) {
-      display: none;
+      & :nth-child(1) {
+        font-size: clamp(1.28rem, calc(1.12rem + 0.48vw), 1.6rem);
+        line-height: 1.6;
+        font-style: italic;
+      }
+      & :nth-child(2) {
+        font-size: clamp(1.1rem, calc(0.96rem + 0.45vw), 1.4rem);
+        font-weight: 100;
+      }
     }
   }
 }
 
-section.active {
-  & h2 {
+section.active .resume {
+  &-heading {
     animation: fadeInUp 0.5s ease-out 0.2s forwards;
   }
-  & .timeline {
+
+  &-timeline {
     animation: fadeInScaleUp 0.3s $easeOutCubic 0.4s forwards;
   }
 
   @for $i from 1 through 4 {
-    & li:nth-of-type(#{$i}) {
+    &-item:nth-of-type(#{$i}) {
       animation: fadeInScaleX 0.3s $easeOutCubic 0.6s forwards;
 
-      & > *:not(div) > * {
+      > *:not(div) > * {
         animation: fadeInUp 0.3s $easeOutCubic 0.9s forwards;
       }
 
-      & div[class~='fa-layers'] {
-        animation: fadeInScaleUp 0.3s $easeOutBack $i * 0.2 + 1s forwards;
+      .circle-icon {
+        animation: fadeInScaleUp
+          0.3s
+          $easeOutBack
+          $i *
+          0.2 +
+          1s
+          forwards;
       }
 
       &::after {
-        animation: fadeInScaleUp 0.3s $easeOutBack $i * 0.2 + 1s forwards;
+        animation: fadeInScaleUp
+          0.3s
+          $easeOutBack
+          $i *
+          0.2 +
+          1s
+          forwards;
       }
     }
   }
